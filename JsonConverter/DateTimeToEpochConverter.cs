70f982a4c2a6f83f641d7a6c92a5c0fc0aa00cf4
mparchin <mmzparchin@gmail.com>
2024-12-02 14:56:24 +0330

    fix epoch converter timezone bug

--- JsonConverter/DateTimeToEpochConverter.cs
+++ JsonConverter/DateTimeToEpochConverter.cs
@@ -7,7 +7,7 @@ namespace MParchin.Authority.JsonConverter;
 public class DateTimeToEpochConverter : JsonConverter<DateTime>
 {
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-        DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64()).DateTime.ToUniversalTime();
+        new(DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64()).DateTime.Ticks, DateTimeKind.Utc);
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) =>
         writer.WriteNumberValue(Convert.ToInt64((value.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalMilliseconds));
