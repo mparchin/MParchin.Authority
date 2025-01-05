4f2f2bc4c3372d1692ad4e9bb963634ba1e5310f
mparchin <mmzparchin@gmail.com>
2025-01-05 19:18:09 +0330

    add policies to service collection

--- Authorization.cs
+++ Authorization.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MParchin.Authority;
 
 public static partial class Authorization
 {
+    private const string UserPolicyName = "User";
     public static AuthorizationPolicy User { get; } = new AuthorizationPolicyBuilder()
         .RequireAuthenticatedUser()
         .Build();
+    private const string AdminPolicyName = "Admin";
     public static AuthorizationPolicy Admin { get; } = new AuthorizationPolicyBuilder()
         .RequireAuthenticatedUser()
         .RequireClaim("role", "admin")
         .Build();
+
+    public static IServiceCollection AddDefaultAuthorization(this IServiceCollection services) =>
+        services.AddAuthorization(options => options.AddDefaultPolicies());
+
+    public static void AddDefaultPolicies(this AuthorizationOptions options)
+    {
+        options.AddPolicy(UserPolicyName, User);
+        options.AddPolicy(AdminPolicyName, Admin);
+    }
 }
